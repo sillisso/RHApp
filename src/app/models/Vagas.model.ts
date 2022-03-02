17e18f3e2f3f31816f0e4afb40c06174186c5c6c
Silvio Fiori <sillisso@gmail.com>
2022-03-02 18:26:20 -0300

    Upload do trabalho do Senai UC 12

---
+++ src/app/models/Vagas.model.ts
@@ -0,0 +1,13 @@
+export class Vaga {
+    id: number = 0;
+    nome: string = "";
+    descricao: string = "";
+    salario: number = 0;
+
+    constructor(id:number,nome:string,descricao: string,salario: number){
+        this.id = id;
+        this.nome= nome;        
+        this.descricao = descricao;
+        this.salario = salario;
+    }
+}
---
+++ src/app/painel-vagas/painel-vagas.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Vaga } from '../models/Vagas.model';
+import { VagasService } from '../vagas.service';
+
+@Component({
+  selector: 'app-painel-vagas',
+  templateUrl: './painel-vagas.component.html',
+  styleUrls: ['./painel-vagas.component.css']
+})
+export class PainelVagasComponent implements OnInit {
+
+  public vaga: Vaga = new Vaga(0,"","",0);
+
+  constructor(private _vagasService: VagasService) { }
+
+  ngOnInit(): void {
+  }
+
+  cadastrar(){
+    this._vagasService.cadastrarVaga(this.vaga).subscribe(
+      vaga => {this.vaga =new Vaga(0,"","",0)},
+      err => {console.log("Erro ao cadastrar")}
+    );
+    window.location.href = "/mural";
+  }
+
+  atualizar(id: number){
+    this._vagasService.atualizarVaga(id,this.vaga).subscribe(
+      vaga => {this.vaga =new Vaga(0,"","",0)},
+      err => {console.log("Erro ao atualizar o cadastro")}
+    );
+    window.location.href = "/mural";
+  }
+
+  excluir(id: number){
+    this._vagasService.removerVaga(id).subscribe(
+      vaga => {this.vaga =new Vaga(0,"","",0)},
+      err => {console.log("Erro ao excluir o cadastro")}
+    );
+    window.location.href = "/mural";
+  }
+}
